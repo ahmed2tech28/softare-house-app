dd154fd7db690df0fd0e231bb660d49859c38be1
ahmed2tech28 <120560296+ahmed2tech28@users.noreply.github.com>
2024-06-03 16:03:05 +0500

    Fixing main page mobile responisve

--- components/common/MapDetailBox.tsx
+++ components/common/MapDetailBox.tsx
@@ -22,18 +22,48 @@ const MapDetailBox: React.FC<Props> = ({
   bg,
 }) => {
   return (
-    <div
-      className="rounded-xl flex flex-col justify-start p-5 bg-white absolute border border-gray-300 shadow-xl"
-      style={{ top, left, height, width, background: `${bg || "white"}` }}
-    >
-      <span
-        className="font-semibold"
-        style={{ fontSize, color: `${bg ? "white" : "black"}` }}
+    <>
+      <div
+        className="rounded-xl hidden flex-col justify-start p-5 bg-white absolute border border-gray-300 shadow-xl xl:flex"
+        style={{ top, left, height, width, background: `${bg || "white"}` }}
       >
-        {hiding}
-      </span>
-      <span style={{ color: `${bg ? "white" : "#969FAF"}` }}>{desc}</span>
-    </div>
+        <span
+          className="font-semibold"
+          style={{ fontSize, color: `${bg ? "white" : "black"}` }}
+        >
+          {hiding}
+        </span>
+        <span style={{ color: `${bg ? "white" : "#969FAF"}` }}>{desc}</span>
+      </div>
+      <div
+        className="rounded-xl hidden flex-col justify-start p-5 bg-white absolute border border-gray-300 shadow-xl xl:hidden lg:flex"
+        style={{
+          top,
+          left: `${
+            parseInt(left.split("p")[0]) > 0 &&
+            parseInt(left.split("p")[0]) < 590
+              ? parseInt(left.split("p")[0]) - 50
+              : parseInt(left.split("p")[0]) >= 590 &&
+                parseInt(left.split("p")[0]) < 975
+              ? parseInt(left.split("p")[0]) - 100
+              : parseInt(left.split("p")[0]) >= 975
+              ? parseInt(left.split("p")[0]) - 150
+              : parseInt(left.split("p")[0])
+          }px`,
+          height,
+          width: `${parseInt(width.split("p")[0]) - 50}px`,
+          background: `${bg || "white"}`,
+        }}
+      >
+        <span
+          className="font-semibold"
+          style={{ fontSize, color: `${bg ? "white" : "black"}` }}
+        >
+          {hiding}
+        </span>
+        <span style={{ color: `${bg ? "white" : "#969FAF"}` }}>{desc}</span>
+      </div>
+    </>
   );
 };
 
--- components/sections/Home/MapArea.tsx
+++ components/sections/Home/MapArea.tsx
@@ -6,6 +6,76 @@ import Map from "../../svgs/Map";
 import MapSmall from "../../svgs/MapSmall";
 import MapTab from "../../svgs/MapTab";
 
+interface mapdetails {
+  left: string;
+  top: string;
+  height: string;
+  width: string;
+  hiding: string;
+  desc: string;
+  fontSize?: string;
+  bg?: string;
+}
+
+const mapdetails: mapdetails[] = [
+  {
+    hiding: "1000+",
+    desc: "projects delivered",
+    height: "146px",
+    width: "308px",
+    top: "0",
+    left: "0px",
+    fontSize: "30px",
+    bg: "#0C8CE9",
+  },
+  {
+    hiding: "2000+",
+    desc: "top tech experts, middle and senior level",
+    height: "146px",
+    width: "235px",
+    top: "103px",
+    left: "208px",
+    fontSize: "30px",
+  },
+  {
+    hiding: "120+",
+    desc: "active clients, including Fortune 500 companies",
+    height: "146px",
+    width: "235px",
+    top: "13px",
+    left: "378px",
+    fontSize: "30px",
+  },
+  {
+    hiding: "33+",
+    desc: "years on the market",
+    height: "146px",
+    width: "308px",
+    top: "103px",
+    left: "590px",
+    fontSize: "30px",
+    bg: "#0C8CE9",
+  },
+  {
+    hiding: "20",
+    desc: "offices and delivery centres globally",
+    height: "146px",
+    width: "235px",
+    top: "-2px",
+    left: "750px",
+    fontSize: "30px",
+  },
+  {
+    hiding: "66",
+    desc: "net promoted score (NPS)",
+    height: "146px",
+    width: "235px",
+    top: "73px",
+    left: "975px",
+    fontSize: "30px",
+  },
+];
+
 const MapArea: React.FC = () => {
   const [mousePositionX, setMousePositionX] = useState(0);
   const [mousePositionY, setMousePositionY] = useState(0);
@@ -88,63 +158,46 @@ const MapArea: React.FC = () => {
           mousePositionY={mousePositionY}
         />
       </div>
-      <div className="h-[277px] 2xl:w-[80%] xl:w-[90%] hidden xl:block mx-auto relative">
-        <MapDetailBox
-          hiding="1000+"
-          desc="projects delivered"
-          height="146px"
-          width="308px"
-          top="0"
-          left="0"
-          fontSize="30px"
-          bg="#0C8CE9"
-        />
-        <MapDetailBox
-          hiding="2000+"
-          desc="top tech experts, middle and senior level"
-          height="146px"
-          width="235px"
-          top="103px"
-          left="208px"
-          fontSize="30px"
-        />
-        <MapDetailBox
-          hiding="120+"
-          desc="active clients, including Fortune 500 companies"
-          height="146px"
-          width="235px"
-          top="13px"
-          left="378px"
-          fontSize="30px"
-        />
-        <MapDetailBox
-          hiding="33+"
-          desc="years on the market"
-          height="146px"
-          width="308px"
-          top="103px"
-          left="590px"
-          fontSize="30px"
-          bg="#0C8CE9"
-        />
-        <MapDetailBox
-          hiding="20"
-          desc="offices and delivery centres globally"
-          height="146px"
-          width="235px"
-          top="-2px"
-          left="750px"
-          fontSize="30px"
-        />
-        <MapDetailBox
-          hiding="66"
-          desc="net promoted score (NPS)"
-          height="146px"
-          width="235px"
-          top="73px"
-          left="975px"
-          fontSize="30px"
-        />
+      <div className="h-[277px] 2xl:w-[80%] xl:w-[90%] lg:w-[90vw] lg:block hidden xl:mx-auto xl:ms-0 ms-[-2rem] relative">
+        {mapdetails.map((item, i) => (
+          <MapDetailBox
+            key={i}
+            hiding={item.hiding}
+            height={item.height}
+            desc={item.desc}
+            left={item.left}
+            top={item.top}
+            width={item.width}
+            bg={item.bg}
+            fontSize={item.fontSize}
+          />
+        ))}
+      </div>
+      <div className="h-fit py-[2rem] flex flex-col w-full lg:hidden  gap-y-[2rem]">
+        {mapdetails.map((item, i) => (
+          <div
+            key={i}
+            style={{
+              height: item.height,
+              width: "100%",
+              background: `${item.bg || "white"}`,
+            }}
+            className="rounded-xl lg:hidden flex-col justify-start p-5 bg-white border border-gray-300 shadow-xl flex"
+          >
+            <span
+              className="font-semibold"
+              style={{
+                fontSize: item.fontSize,
+                color: `${item.bg ? "white" : "black"}`,
+              }}
+            >
+              {item.hiding}
+            </span>
+            <span style={{ color: `${item.bg ? "white" : "#969FAF"}` }}>
+              {item.desc}
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
