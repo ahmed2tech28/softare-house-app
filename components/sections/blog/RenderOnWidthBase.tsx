c0a1cf4d27fdbb92598f4a64aa3b5a7c4ed68ed4
ahmed2tech28 <ahmedalirana28@gmail.com>
2024-07-05 20:02:00 +0500

    Adding fixes to render with base

--- components/sections/blog/RenderOnWidthBase.tsx
+++ components/sections/blog/RenderOnWidthBase.tsx
@@ -5,19 +5,21 @@ import BlogAreaMob from "./BlogAreaMob";
 
 const RenderOnWidthBase = () => {
   const [showView, setShowView] = useState<"mobile" | "desktop">("desktop");
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (window.innerWidth < 1024) {
-        setShowView("mobile");
-      } else if (window.innerWidth >= 1024) {
-        setShowView("desktop");
-      }
-    }
-  }, [window.innerWidth, showView]);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     if (window.innerWidth < 1024) {
+  //       setShowView("mobile");
+  //     } else if (window.innerWidth >= 1024) {
+  //       setShowView("desktop");
+  //     }
+  //   }
+  // }, [window.innerWidth, showView]);
   return (
     <>
-      {showView == "desktop" && <BlogArea />}
-      {showView == "mobile" && <BlogAreaMob />}
+      {/* {showView == "desktop" && <BlogArea />}
+      {showView == "mobile" && <BlogAreaMob />} */}
+      <BlogArea />
+      <BlogAreaMob />
     </>
   );
 };
